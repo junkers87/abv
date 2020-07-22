883028b74cc6287efc1aa054b3cce6e6d0cfbdd3
junkers87 <furkanakay13@gmail.com>
2020-07-22 19:30:35 +0300

    okudum anladım onaylıyorum 2

--- formatting_string.py
+++ formatting_string.py
@@ -1,2 +1,7 @@
 print('this is a string {}'.format('INSERTED'))
 print('british main battle tanks are {2} {0} and {1}'.format('churchill' , 'challenger','mathilda'))
+#british main battle tanks are mathilda churchill and challenger
+print('british main battle tanks are {0} {0} and {0}'.format('churchill' , 'challenger','mathilda'))
+#british main battle tanks are churchill churchill and churchill
+print('british main battle tanks are {a} {b} and {b}'.format(a='churchill' , b='challenger',c='mathilda'))
+#british main battle tanks are churchill challenger and challenger
