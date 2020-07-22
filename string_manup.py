4bb39f5cb942d1eab56cf6f04bfee5561359620f
junkers87 <furkanakay13@gmail.com>
2020-07-22 17:15:48 +0300

    okudum anladım onaylıyorum 2

--- string_manup.py
+++ string_manup.py
@@ -1,4 +1,13 @@
 print(len('acsgşdfdsfk'))
+# print lenght of acsgşdfdsfk string
+mystring = "helloworld"
+print(mystring[2])
+#print second letter of helloworld string
 
-mystring = "hello world"
-print(mystring[-2])
+print(mystring[2:])
+
+# 2: =  slice after second letter (lloworld)
+
+print(mystring[:4])
+
+# print first 4 letter (hell)
