7ae4a117da7359563ea5c797ca4f0e6030bfe907
junkers87 <furkanakay13@gmail.com>
2020-07-22 19:37:43 +0300

    okudum anladım onaylıyorum 2

--- formatting_string.py
+++ formatting_string.py
@@ -5,3 +5,11 @@ print('british main battle tanks are {0} {0} and {0}'.format('churchill' , 'chal
 #british main battle tanks are churchill churchill and churchill
 print('british main battle tanks are {a} {b} and {b}'.format(a='churchill' , b='challenger',c='mathilda'))
 #british main battle tanks are churchill challenger and challenger
+
+result = 1/7.77
+print(result)
+
+print('the result was {}'.format(result))
+
+#float formatting "{value:width.presicion f}"
+print('the result was {r}'.format(r=result))
