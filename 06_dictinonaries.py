f51b50b5d0ff745e5a0c9e76b5a3162ed6afe03b
junkers87 <furkanakay13@gmail.com>
2020-07-23 18:43:04 +0300

    okudum anladım onaylıyorum 2

---
+++ 06_dictinonaries.py
@@ -0,0 +1,14 @@
+# {'key1':'value1','key2':value2'}
+#dictionaries can not be sorted can retrieved by key
+#lists can be sorted and can retrieved by location. ordered sequences
+# can be indexed or sliced
+# dictionaries retrieve value faster without location
+my_dictionary =  {'key1':'value1','key2':'value2'}
+print(my_dictionary)   #prints the whole dictionary
+print(my_dictionary['key1']) #prints the value1
+
+prices = {"c180":'25000',"c200":'28000',"c220":'32500'}
+
+
+print(prices[input('bir model seçiniz')]+"$")
+
