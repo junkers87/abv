4fefe6b6217a99879c3aa3adb24f7df9fe4baff0
junkers87 <furkanakay13@gmail.com>
2020-07-25 15:52:54 +0300

    okudum anladım onaylıyorum 2

---
+++ 08_sets.py
@@ -0,0 +1,9 @@
+# only unique items can be used so any item can be used only once
+my_set = set()
+print(my_set)
+my_set.add(1)
+print(my_set)
+my_set.add(2)
+print(my_set)
+my_list = [2,2,2,2,33,3,3,3,3,3,4,4,4,4,4,5,5,5,5]
+print(set(my_list))
